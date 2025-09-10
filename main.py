4991ffea1be4b50165b01403fbaa2d17615c9a48
Bhagyesh Dhanashre <138644030+BhagyeshDhanashre@users.noreply.github.com>
2025-09-10 18:09:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-app=FastAPI()
+app = FastAPI()
 
 @app.get("/")
-def get_req():
-    return "Hello World"
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
